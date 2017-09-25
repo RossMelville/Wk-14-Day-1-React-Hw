ee16e5d1fdcc661b961ddc6ff5a08c85fe1ff2ba
Ross Melville <rossmelville@hotmail.com>
2017-09-25 18:00:17 +0100

    Added a times to the state property

--- client/src/components/CommentBox.jsx
+++ client/src/components/CommentBox.jsx
@@ -7,13 +7,13 @@ class CommentBox extends React.Component {
     super(props)
     this.state = {
       data: [
-        { film: 'Sausage Party'}, 
-        { film: 'Cafe Society'},
-        { film: 'Morgan'},
-        { film: 'The 9th Life of Louis Drax'},
-        { film: 'Naam Hai Akira'},
-        { film: 'Equity'},
-        { film: 'Things to Come'}
+        { film: 'Sausage Party', times: [13.30, 17.45]}, 
+        { film: 'Cafe Society', times: [18.15, 20.45]},
+        { film: 'Morgan', times: [16.40, 19.45]},
+        { film: 'The 9th Life of Louis Drax', times: [18.00, 21.00]},
+        { film: 'Naam Hai Akira', times: [19.30, 21.45]},
+        { film: 'Equity', times: [18.55, 22.00]},
+        { film: 'Things to Come', times: [19.15, 21.10]}
       ]
     }
   }
