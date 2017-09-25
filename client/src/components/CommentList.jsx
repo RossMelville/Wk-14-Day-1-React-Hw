5ec0ef30f39201f7cc524b4f8eecc484f251b190
Ross Melville <rossmelville@hotmail.com>
2017-09-25 16:12:43 +0100

    Added render function to CommentList.jsx

--- client/src/components/CommentList.jsx
+++ client/src/components/CommentList.jsx
@@ -5,10 +5,20 @@ class CommentList extends React.Component {
 
   render() {
 
-    
+    const commentNodes = this.props.data.map ( (comment, index) => {
+      return (
+        <Comment film={comment.film} key={index}>
+          {comment.film}
+        </Comment>
+      )
+    })
+
+    return (
+      <div className="comment-list">
+        {commentNodes}
+      </div>
+    )
   }
-
-
 }
 
 export default CommentList
